7dd2902e4f1f3e789be83dba3b51329e87d5654a
Quan <quan199877@gmail.com>
2018-10-05 11:26:46 +0700

    Test case simple calculator

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int sub(int a, int b) {
+        return a - b;
+    }
+}
